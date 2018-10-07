a5d7b3ac34f26c69367c5cf4fdf92cdb119d76e2
MaryChCronin <43207966+MaryChCronin@users.noreply.github.com>
2018-10-07 16:01:15 +0100

    video 3 Model

--- Tic-Tac-Toe/app/src/main/java/com/example/maryc/tictactoe/MainActivity.java
+++ Tic-Tac-Toe/app/src/main/java/com/example/maryc/tictactoe/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.maryc.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
     private TicTacToeGame mGame;
@@ -11,7 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGame =new TicTacToeGame(this);
+        mGame = new TicTacToeGame(this);
 
     }
-}
+        public void pressedSquare(View view){
+            String tagAsString = view.getTag().toString();
+            int tagAsInt = Integer.parseInt(tagAsString);
+            //Log.d("TTT", "You pressed index " + tagAsInt);
+            //Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+            mGame.pressedButtonAtIndex(tagAsInt);
+            //updateView();
+
+        }
+    public void pressNewGame (View view)
+    {
+        //Toast.makeText(this, "New Game",Toast.LENGTH_SHORT).show();
+        mGame   = new TicTacToeGame(this);
+       // updateView();
+    }
+
+
+    }
+
