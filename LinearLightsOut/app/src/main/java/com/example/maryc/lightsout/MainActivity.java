6d6435cc81b68d13f429520d83068ddc004c4248
MaryChCronin <43207966+MaryChCronin@users.noreply.github.com>
2018-10-09 00:03:29 +0100

    final

--- LinearLightsOut/app/src/main/java/com/example/maryc/lightsout/MainActivity.java
+++ LinearLightsOut/app/src/main/java/com/example/maryc/lightsout/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.maryc.lightsout;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private LightsOutGame mGame;
+    private TextView mGameStateTextView;
+    private Button[] mButtons;
+    private int mNumButtons = 7;
+    String tagAsString;
+    int tagAsInt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGame   = new LightsOutGame(mNumButtons);
+        mGameStateTextView = findViewById(R.id.start);
+        mButtons = new Button[mNumButtons];
+        mButtons[0] = findViewById(R.id.button0);
+        mButtons[1] = findViewById(R.id.button1);
+        mButtons[2] = findViewById(R.id.button2);
+        mButtons[3] = findViewById(R.id.button3);
+        mButtons[4] = findViewById(R.id.button4);
+        mButtons[5] = findViewById(R.id.button5);
+        mButtons[6] = findViewById(R.id.button6);
+
+        updateView();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+    public void pressedLight (View view) {
+        tagAsString = view.getTag().toString();
+        tagAsInt = Integer.parseInt(tagAsString);
+        Log.d("TTT", "You pressed index " + tagAsInt);
+        Toast.makeText(this, "You pressed index " + tagAsInt, Toast.LENGTH_SHORT).show();
+        mGame.pressedButtonAtIndex(tagAsInt);
+        updateView();
+    }
+    public void pressNewGame (View view)
+    {
+        //Toast.makeText(this, "New Game",Toast.LENGTH_SHORT).show();
+        mGame   = new LightsOutGame(mNumButtons);
+        updateView();
+        for (int i=0;i < mNumButtons ;i++)
+        {
+            mButtons[i].setEnabled(true);
+           // mButtons[i].setBackgroundColor(getResources().getColor(R.color.buttonBackground));
+        }
+        mGameStateTextView.setText(R.string.start);
+    }
+    public void updateView()
+    {
+        for (int i=0;i < mNumButtons ;i++)
+        {
+            mButtons[i].setText(Integer.toString(mGame.getValueAtIndex(i)));
+        }
+        if (mGame.checkForWin() == true)
+        {
+            mGameStateTextView.setText("You have won!");
+            for (int i=0;i < mNumButtons;i++)
+            {
+                mButtons[i].setEnabled(false);
+                //mButtons[i].setBackgroundColor(getResources().getColor(R.color.buttonBackgroundDisabled));
+            }
+
+        } else {
+            if (mGame.getNumPresses() == 1)
+                mGameStateTextView.setText(getString(R.string.first_turn));
+            else
+                mGameStateTextView.setText(getString(R.string.turn_count, mGame.getNumPresses() ));
+        }
+
+
     }
 }
