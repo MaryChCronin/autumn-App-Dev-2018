2a732d4ca4e255692dc2c66b0976d11512d9c660
MaryChCronin <43207966+MaryChCronin@users.noreply.github.com>
2018-10-19 11:39:35 +0100

    dialog

--- Point_of_sale/app/src/main/java/com/example/maryc/pointofsale/MainActivity.java
+++ Point_of_sale/app/src/main/java/com/example/maryc/pointofsale/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.maryc.pointofsale;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mNameTextView, mQuantityTextView, mDateTextView;
@@ -34,10 +41,38 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCurrentItem = Item.getDefaultItem();
-                showCurrentItem();        
+                addItem();      
                             }
         });
+    }
+
+    private void addItem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_add, null, false);
+        builder.setView(view);
+        final EditText nameEditText = (EditText) view.findViewById(R.id.edit_name);
+        final EditText quantityEditText = (EditText) view.findViewById(R.id.edit_quantity);
+        final CalendarView deliveryDateView = (CalendarView) view.findViewById(R.id.calendar_view);
+        final GregorianCalendar calendar = new GregorianCalendar();
+        deliveryDateView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                calendar.set(year, month, dayOfMonth);
+            }
+        });
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = nameEditText.getText().toString();
+                int quantity = Integer.parseInt(quantityEditText.getText().toString());
+                mCurrentItem = new Item(name, quantity, calendar);
+
+                showCurrentItem();
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+
     }
 
     private void showCurrentItem() {
