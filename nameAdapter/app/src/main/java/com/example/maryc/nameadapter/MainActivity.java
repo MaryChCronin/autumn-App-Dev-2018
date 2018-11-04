9c39fdff234a9a761f36210f97139a061be53b92
MaryChCronin <43207966+MaryChCronin@users.noreply.github.com>
2018-11-04 13:26:12 +0000

    grid

--- nameAdapter/app/src/main/java/com/example/maryc/nameadapter/MainActivity.java
+++ nameAdapter/app/src/main/java/com/example/maryc/nameadapter/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //recyclerView.setLayoutManager(new GridLayoutManager(this,2));
         recyclerView.setHasFixedSize(true);
         final NameAdapter nameAdapter = new NameAdapter();
         recyclerView.setAdapter(nameAdapter);
